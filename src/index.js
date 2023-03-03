65a1fe95cb2cda112edb650ff6c2ad329e6cce3e
nubisub <hendraksm18@mail.ugm.ac.id>
2023-03-03 22:10:51 +0700

    add CRUD operation

--- src/index.js
+++ src/index.js
@@ -1,21 +1,59 @@
+const fs = require("fs");
+const path = require("path");
+
 const { ApolloServer } = require("apollo-server");
 
 // 1
-const typeDefs = `
-type Query {
-    info: String!
-}`;
+
+let links = [
+	{
+		id: "link-0",
+		url: "www.howtographql.com",
+		description: "Fullstack tutorial for GraphQL",
+	},
+];
 
 // 2
 const resolvers = {
 	Query: {
 		info: () => `This is the API of a Hackernews Clone`,
+		feed: () => links,
+	},
+
+	Link: {
+		id: (parent) => parent.id,
+		description: (parent) => parent.description,
+		url: (parent) => parent.url,
+	},
+	Mutation: {
+		post: (parent, args) => {
+			let idCount = links.length;
+
+			const link = {
+				id: `link-${idCount++}`,
+				description: args.description,
+				url: args.url,
+			};
+			links.push(link);
+			return link;
+		},
+		updateLink: (parent, args) => {
+			let link = links.find((link) => link.id === args.id);
+			link.url = args.url;
+			link.description = args.description;
+			return link;
+		},
+		deleteLink: (parent, args) => {
+			let link = links.find((link) => link.id === args.id);
+			links = links.filter((link) => link.id !== args.id);
+			return link;
+		},
 	},
 };
 
 // 3
 const server = new ApolloServer({
-	typeDefs,
+	typeDefs: fs.readFileSync(path.join(__dirname, "schema.graphql"), "utf8"),
 	resolvers,
 });
 
